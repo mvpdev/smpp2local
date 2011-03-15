9d869f3b8a2db331c0b3a457a46d3490bf80c01d
rgaudin <rgaudin@gmail.com>
2011-03-15 12:58:20 +0000

    added log and exception handling

--- kanouchd.py
+++ kanouchd.py
@@ -6,17 +6,18 @@
 
 import sys
 from datetime import datetime
+import logging as log
 
 import cherrypy
 import couchdb
 
 from config import *
 
-couch = couchdb.Server(COUCH_SERVER)
-database = couch[COUCH_DB]
+log.basicConfig(level=log.INFO, filename=KANOUCHD_LOG_FILE)
 
 
 def document_from_sms(identity, text, date=datetime.now()):
+    ''' returns dictionary matching CouchDB and RSMS backend format '''
     document = {'identity': identity,
                 'datetime': date.isoformat(),
                 'text': text,
@@ -24,6 +25,20 @@ def document_from_sms(identity, text, date=datetime.now()):
                 'status': 'created'}
     return document
 
+
+def save_document_to_couch(document):
+    ''' saves an arbitrary document to CouchDB '''
+
+    # connect DB upon message reception.
+    # this is not very good for performance
+    # but it allows CouchDB restarts without effects.
+    couch = couchdb.Server(COUCH_SERVER)
+    database = couch[COUCH_DB] 
+
+    # create a Couch document and save it to DB.
+    doc_id = database.create(document)
+    doc = database[doc_id]
+    return doc
 
 class SMSReceiver:
 
@@ -37,9 +52,16 @@ class SMSReceiver:
         if not identity or not text:
             return
 
-        doc_id = database.create(document_from_sms(identity, \
-                                                   text, date=datetime.now()))
-        doc = database[doc_id]
+        document = document_from_sms(identity, text, date=datetime.now())
+
+        try:
+            doc = save_document_to_couch(document)
+        except:
+            # I believe this should prevent kannel from discarding the
+            # incoming sms and add it to retry spool.
+            log.critical("can't connect or create document on CouchDB.")
+            raise cherrypy.HTTPError(500, u"Server could not create message" \
+                                           " into CouchDB.")
 
         return doc
 
@@ -50,4 +72,14 @@ def main():
                         {'global': {'server.socket_port': KANOUCHD_PORT}})
 
 if __name__ == '__main__':
-    main()
+    try:
+        log.info("started.")
+        main()
+        log.info("soft shutdown.")
+        shutdown()
+    except KeyboardInterrupt:
+        log.warning("shutdown with ^C.")
+        shutdown()
+    except Exception, e:
+        log.error("shutdown by exception: %r" % e)
+        shutdown()
