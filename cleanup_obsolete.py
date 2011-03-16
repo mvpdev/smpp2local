72e9051470b3dddd608c84e1e9b4594b173e599d
rgaudin <rgaudin@gmail.com>
2011-03-16 14:25:30 +0000

    added cleanup script

---
+++ cleanup_obsolete.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# encoding=utf-8
+# maintainer: rgaudin
+
+''' removes CouchDB messages older than specified delta
+
+    This script is supposed to be run nightly. '''
+
+import re
+import datetime
+
+import couchdb
+
+from config import *
+
+# global variables holding CouchDB connection and logger
+log.basicConfig(level=log.INFO, filename=CLEANUP_LOG_FILE)
+
+try:
+    couch = couchdb.Server(COUCH_SERVER)
+    database = couch[COUCH_DB]
+except IndexError:
+    die("database %s doesn't exist in CouchDB" % COUCH_DB)
+except:
+    die("CouchDB is not started.")
+
+
+def get_messages_from_couch():
+    ''' fecthes all processed messages in couch and return them '''
+    messages = []
+    delta = datetime.timedelta(CLEANUP_TIMEDELTA)
+    today = datetime.datetime.now()
+
+    # uses permanent view if defined
+    # use a temporary view if not.
+    if COUCH_PROCESSED_VIEW:
+        results = database.view(COUCH_PROCESSED_VIEW)
+    else:
+        map_fun = '''function(doc) {
+            if (doc.direction && doc.status == 'processed')
+                emit(doc, null);
+            }'''
+        results = database.query(map_fun)
+    for row in results:
+        message = row.key
+        try:
+            date = message_date = datetime.datetime(*map(int, \
+                                                         re.split('[^\d]', \
+                                                    message['datetime'])[:-1]))
+        except KeyError:
+            # if it has no datetime, it's not
+            # for us anyway.
+            continue
+
+        if (today - delta) > date:
+            messages.append(message)
+
+    return messages
+
+
+def delete_document_from_couch(document_id):
+    doc = database[document_id]
+    database.delete(doc)
+
+
+def main():
+    messages = get_messages_from_couch()
+    log.info("%d messages to delete from CouchDB" % messages.__len__())
+    for message in messages:
+        log.info("Deleting message %s" % message['_id'])
+        try:
+            delete_document_from_couch(message['_id'])
+        except Exception as e:
+            log.error("failed to delete %s from CouchDB. %r" \
+                      % (message['_id'], e))
+
+
+if __name__ == '__main__':
+    try:
+        log.info("started.")
+        main()
+        log.info("soft shutdown.")
+        shutdown()
+    except KeyboardInterrupt:
+        log.warning("shutdown with ^C.")
+        shutdown()
+    except Exception, e:
+        log.error("shutdown by exception: %r" % e)
+        shutdown()
--- config.py
+++ config.py
@@ -4,14 +4,23 @@
 
 import logging as log
 
+# time to wait (in second) before
+# rechecking stdin in counell script
 SLEEP_INTERVAL = 1
+# number of SLEEP_INTERVAL to wait
+# before rechecking is couch is still alive
 COUCH_CHECK_INTERVAL = 20
+# number of days old a processed message
+# should be to be deleted
+CLEANUP_TIMEDELTA = 1
 COUNELL_LOG_FILE = '/tmp/counell.log'
 KANOUCHD_LOG_FILE = '/tmp/kanouchd.log'
+CLEANUP_LOG_FILE = '/tmp/smpp_cleanup.log'
 COUCH_SERVER = 'http://localhost:5984/'
 COUCH_DB = 'cc_sms'
 COUCH_KANNEL_VIEW = 'cc/kannel'
 COUCH_SMS_VIEW = 'cc/rapidsms'
+COUCH_PROCESSED_VIEW = 'cc/processed'
 COUCH_PID_FILE = '/var/run/couchdb/couchdb.pid'
 KANOUCHD_PORT = 8090
 KANNEL_SERVER = 'localhost'
@@ -37,6 +46,7 @@ KANNEL_PATH = "/cgi-bin/sendsms?username=" \
                 'identity': '%(identity)s', 'text': '%(text)s', \
                 'charset': KANNEL_CHARSET, 'coding': KANNEL_CODING}
 KANNEL_URL = "http://%s%s" % (KANNEL_SERVER_STRING, KANNEL_PATH)
+
 
 def shutdown(status=0):
     ''' ensure proper shutdown '''
